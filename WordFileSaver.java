08436ee70fef7fc9310ab26b0ccbc3afd760fe78
taeminkim-408 <82189651+taeminkim-408@users.noreply.github.com>
2023-09-16 13:59:14 +0900

    Update and rename WordFileLoader to WordFileSaver.java

---
+++ WordFileSaver.java
@@ -0,0 +1,21 @@
+package com.taemin.word;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class WordFileSaver {
+    public void saveFile(String filePath, ArrayList<Word> list) {
+        try {
+            PrintWriter pr = new PrintWriter(new FileWriter(filePath));
+            for (Word one : list) {
+                pr.write(one.toFileString() + "\n");
+            }
+            pr.close();
+            System.out.println("==> 데이터 저장완료!!!!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
